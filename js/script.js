0fb5f11cdd9330c7f6ee016674d97ee010efc1dd
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-08-04 15:37:34 +0200

    stop when a player win

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 let player = 1;
-
-// document.getElementById("container").addEventListener("click", addSign);
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
 
 function addSign(div){
-        if(player === 1){
-            document.getElementById("player_nb").textContent = 2;
-            div.target.textContent = "X";
-            player = 2;
-        }
-        else{
-            document.getElementById("player_nb").textContent = 1;
-            div.target.textContent = "O";
-            player = 1;
-        }
+    if(player === 1){
+        div.target.textContent = "X";
         console.log(div.target);
+        document.getElementById("player_nb").textContent = 2;
+        isWin();
+        player = 2;
+    }
+    else{
+        div.target.textContent = "O";
+        console.log(div.target);
+        document.getElementById("player_nb").textContent = 1;
+        isWin();
+        player = 1;
+    }
     div.target.removeEventListener("click", addSign);
 }
 
@@ -21,4 +31,21 @@ const divList = document.querySelectorAll(".inside");
 
 for(const div of divList){
     div.addEventListener("click", addSign);
+}
+
+function isWin(){
+    console.log(one.textContent===two.textContent && two.textContent===three.textContent);
+    if(one.textContent === two.textContent && two.textContent === three.textContent && one.textContent !== "" ||
+        four.textContent === five.textContent && five.textContent === six.textContent && four.textContent !== "" ||
+        seven.textContent === eight.textContent && eight.textContent === nine.textContent && seven.textContent !== "" ||
+        one.textContent === four.textContent && four.textContent === seven.textContent && one.textContent !== "" ||
+        two.textContent === five.textContent && five.textContent === eight.textContent && two.textContent !== "" ||
+        three.textContent === six.textContent && six.textContent === nine.textContent && three.textContent !== "" ||
+        one.textContent === five.textContent && five.textContent === nine.textContent && one.textContent !== "" ||
+        three.textContent === five.textContent  && five.textContent === seven.textContent && three.textContent !== ""){
+            document.getElementById("player").textContent = `Le joueur ${player} a gagné !`;
+            for(const div of divList){
+                div.removeEventListener("click", addSign);
+            }
+        }
 }
